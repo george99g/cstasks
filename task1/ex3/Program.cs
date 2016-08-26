080d25bfa539547979e6527e1fb07f9ddb7b66cd
george99g <georgi_99g@abv.bg>
2016-08-26 14:35:23 +0300

    task1/ex3

---
+++ task1/ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a string to check if it's a palindrome: ");
+            String input = Console.ReadLine();
+            for(int i = 0; i < input.Length/2; i++)
+                if(input[i] != input[input.Length-1-i])
+                {
+                    Console.WriteLine("no");
+                    return;
+                }
+            Console.WriteLine("yes");
+            return;
+        }
+    }
+}
