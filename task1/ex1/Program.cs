1081f5386f6bf6819e287f242f675f5b39eeba95
george99g <georgi_99g@abv.bg>
2016-08-26 14:35:03 +0300

    task1/ex1

---
+++ task1/ex1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Number of Fibonacci numbers to output: ");
+            int count;
+            while(true)
+            {
+                try {
+                    count = int.Parse(Console.ReadLine());
+                    break;
+                } catch (FormatException) {
+                    Console.Write("Not a valid number. Try again: ");
+                }
+            }
+            int first = 1;
+            int second = 1;
+            int next;
+            for(int i = 0; i < count; i++)
+            {
+                switch(i)
+                {
+                    case 0:
+                        Console.Write(0);
+                        break;
+                    case 1:
+                    case 2:
+                        Console.Write(1);
+                        break;
+                    default:
+                        next = first + second;
+                        second = first;
+                        first = next;
+                        Console.Write(next%2==0?-next:next);
+                        break;
+                }
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            return;
+        }
+    }
+}
