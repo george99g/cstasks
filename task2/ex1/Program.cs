e7cf072b705ced9c38b93be9333c149221c96e54
george99g <georgi_99g@abv.bg>
2016-08-29 18:48:27 +0300

    task2/ex1

---
+++ task2/ex1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static bool CheckParentheses(string input)
+        {
+            int parenCounter = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '(')
+                    parenCounter++;
+                else if(input[i] == ')')
+                {
+                    if(parenCounter <= 0)
+                        return false;
+                    else
+                        parenCounter--;
+                }
+            }
+            return parenCounter == 0;
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Input some text with parentheses: ");
+            string input = Console.ReadLine();
+            if(CheckParentheses(input))
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+            return;
+        }
+    }
+}
