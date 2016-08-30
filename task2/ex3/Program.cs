a40bdc7f0849f0b26c4a3d1e29a9423723ca0443
george99g <georgi_99g@abv.bg>
2016-08-30 21:38:47 +0300

    task2/ex3

---
+++ task2/ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static string Reverse(string input)
+        {
+            string output = "";
+            for(int i = input.Length - 1; i >= 0; i--)
+                output += input[i];
+            return output;
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a string to be reversed: ");
+            Console.WriteLine(Reverse(Console.ReadLine()));
+        }
+    }
+}
