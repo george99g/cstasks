d729f24f0569020b21d983ee3cd0f9291b07cc02
george99g <georgi_99g@abv.bg>
2016-08-31 20:57:01 +0300

    task2/ex5

---
+++ task2/ex5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Tree<Type>
+    {
+        public Tree(Type value)
+        {
+            Root = new TreeNode<Type>(value);
+        }
+        public TreeNode<Type> Root {get; set;}
+    }
+    public class TreeNode<Type>
+    {
+        public TreeNode(Type value)
+        {
+            Value = value;
+        }
+        public void AddChild(TreeNode<Type> node)
+        {
+            _children.Add(node);
+            node._parent = this;
+        }
+        public void AddChild(Type value)
+        {
+            AddChild(new TreeNode<Type>(value));
+        }
+        public void RemoveChild(TreeNode<Type> node)
+        {
+            _children.Remove(node);
+        }
+        public List<TreeNode<Type>> Children
+        {
+            get
+            {
+                return _children;
+            }
+        }
+        List<TreeNode<Type>> _children = new List<TreeNode<Type>>();
+        public TreeNode<Type> Parent
+        {
+            get
+            {
+                return _parent;
+            }
+        }
+        TreeNode<Type> _parent;
+        public Type Value {get; set;}
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
